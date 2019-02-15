7c875517e62edfb41474182ca84a805b839f053e
Yoberty Alejandro Garcia Valera <gyoberty@gmail.com>
2019-02-15 17:24:36 -0400

    Add mutable counter function

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _redux = require('redux');
+// import { createStore } from 'redux'
 
 var _expect = require('expect');
 
@@ -8,42 +8,16 @@ var _expect2 = _interopRequireDefault(_expect);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var counter = function counter() {
-  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-  var action = arguments[1];
-
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+var addCounter = function addCounter(list) {
+  list.push(0);
+  return list;
 };
 
-var store = (0, _redux.createStore)(counter);
+var testAddCounter = function testAddCounter() {
+  var listBefore = [];
+  var listAfter = [0];
+  (0, _expect2.default)(addCounter(listBefore)).toEqual(listAfter);
+};
 
-/*
-
-Counter Tests
-
-expect(
-  counter(0, { type: 'INCREMENT' })
-).toEqual(1)
-expect(
-  counter(1, { type: 'INCREMENT' })
-).toEqual(2)
-expect(
-  counter(2, { type: 'DECREMENT' })
-).toEqual(1)
-expect(
-  counter(1, { type: 'DECREMENT' })
-).toEqual(0)
-*/
-
-store.subscribe(function () {
-  console.log(store.getState());
-});
-
-store.dispatch({ type: 'INCREMENT' });
+testAddCounter();
+console.log('All tests passed');
--- index.js
+++ index.js
@@ -1,41 +1,20 @@
 'use strict'
 
-import { createStore } from 'redux'
-// import expect from 'expect'
+// import { createStore } from 'redux'
+import expect from 'expect'
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
+const addCounter = (list) => {
+  list.push(0)
+  return list
 }
 
-const store = createStore(counter)
+const testAddCounter = () => {
+  const listBefore = []
+  const listAfter = [0]
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter)
+}
 
-store.subscribe(() => {
-  console.log(store.getState())
-})
-
-store.dispatch({ type: 'INCREMENT' })
-
-/*
-
-Counter Tests
-
-expect(
-  counter(0, { type: 'INCREMENT' })
-).toEqual(1)
-expect(
-  counter(1, { type: 'INCREMENT' })
-).toEqual(2)
-expect(
-  counter(2, { type: 'DECREMENT' })
-).toEqual(1)
-expect(
-  counter(1, { type: 'DECREMENT' })
-).toEqual(0)
-*/
+testAddCounter()
+console.log('All tests passed')
