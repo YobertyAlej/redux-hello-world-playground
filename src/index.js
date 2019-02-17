12151e493eff1b886da09aa312fd5f9c8977fdbf
Yoberty Alejandro Garcia Valera <gyoberty@gmail.com>
2019-02-16 20:20:48 -0400

    initial state

--- src/index.js
+++ src/index.js
@@ -1,3 +1,139 @@
 'use strict'
 
-import FormContainer from './js/components/container/FormContainer.jsx'
+import { createStore } from 'redux'
+import expect from 'expect'
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state
+      }
+
+      return Object.assign({}, state, {
+        completed: !state.completed
+      })
+    default:
+      return state
+  }
+}
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action))
+    default:
+      return state
+  }
+}
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
+    default:
+      return state
+  }
+}
+
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        )
+        return nextState
+      },
+      {}
+    )
+  }
+}
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
+
+const testAddTodo = () => {
+  const stateBefore = []
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  }
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ]
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter)
+}
+
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ]
+
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1,
+    text: 'Go shopping',
+    completed: true
+  }
+
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ]
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter)
+}
+
+const store = createStore(todoApp)
+
+testAddTodo()
+testToggleTodo()
+
+console.log('All tests passed')
+console.log('------------')
+console.log(store.getState())
